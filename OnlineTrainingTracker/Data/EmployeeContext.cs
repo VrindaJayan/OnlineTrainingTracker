bcd92405c90d439f239d869fd31dffd92fef13f1
vrinda.jayan <vrinda.jayan@hp.com>
2022-10-07 10:22:58 +0530

    initialcommit

---
+++ OnlineTrainingTracker/Data/EmployeeContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OnlineTrainingTracker.Models;
+namespace OnlineTrainingTracker.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<EmployeeDetails> EmployeeDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<EmployeeDetails>().ToTable("EmployeeDetails");
+        }
+    }
+}
